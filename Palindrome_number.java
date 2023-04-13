37af2658e5af5617335bca45de690f547b419de8
niraj123744 <nirajnirmal661@gmail.com>
2023-04-13 19:22:46 +0530

    [Niraj] Add: This script is used for creating program palindrome number

---
+++ Palindrome_number.java
@@ -0,0 +1,25 @@
+package com.myprogram;
+
+public class Palindrome_number {
+
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+int r, sum = 0, temp;
+int n = 454;// It is the number variable to be checked for palindrome
+
+temp = n;
+while (n > 0) {
+r = n % 10; // getting remainder
+sum = (sum * 10) + r;
+n = n / 10;
+}
+if (temp == sum)
+System.out.println("It is a palindrome number ");
+else
+System.out.println("It is not not a palindrome number");
+}
+}
+	
+
+
