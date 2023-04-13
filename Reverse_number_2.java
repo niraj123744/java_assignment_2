3eea09282820becc9c1d8d32f6b7b5c42fceb653
niraj123744 <nirajnirmal661@gmail.com>
2023-04-13 19:28:33 +0530

    [Niraj] Add: This script is used for creating program reverse number 2

---
+++ Reverse_number_2.java
@@ -0,0 +1,20 @@
+package com.myprogram;
+
+public class Reverse_number_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number = 987654, reverse = 0;  
+		while(number != 0)   
+		{  
+		int remainder = number % 10;  
+		reverse = reverse * 10 + remainder;  
+		number = number/10;  
+		}  
+		System.out.println("The reverse of the given number is: " + reverse);  
+		}  
+		
+	}
+
+
